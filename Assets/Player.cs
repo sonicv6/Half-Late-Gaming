2472f7ca65ba4189c56e0e107af1a2f1154fa943
sonicv6 <masterssword007@yahoo.co.uk>
2023-02-10 23:14:30 +0000

    Ned shit.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,24 +4,55 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private bool groundedPlayer;
+    private Vector3 playerVelocity;
+    private int playerSpeed = 10;
+    private CharacterController controller;
+    private int jumpHeight = 2;
+    private float gravityValue = -9.81f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.g)
-        if (Input.GetKeyDown(KeyCode.W))
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
         {
-            this.gameObject.GetComponent<Rigidbody>().velocity = 5 * Vector3.forward;
+            playerVelocity.y = 0f;
         }
 
-        if (Input.GetKeyUp(KeyCode.W))
+        Vector3 move = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
         {
-            this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            move.z += 1;
         }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            move.z -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            move.x -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            move.x += 1;
+        }
+
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (Input.GetKey(KeyCode.Space) && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
